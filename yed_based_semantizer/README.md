66974dc189e43e68ada9ec90728e1d0375610f47
Denis Shirshov <denis.shirshov@gmail.com>
2025-02-01 19:51:11 +0300

    Implement VAD graphml parsing, write Readme for VAD workflow

--- yed_based_semantizer/README.md
+++ yed_based_semantizer/README.md
@@ -2,7 +2,52 @@
 
 Данный прототип демонстрирует возможности создания бизнес-диаграмм в редакторе yEd и их дальнейший парсинг в триплеты RDF.
 
-## Запуск прототипа
+## Получаем ttl файл из VAD диаграммы
 
-В текущей директории выполнить:
-`python extract_triples.py`
+### Скачиваем файлы проекта к себе локально
+
+Можно скачать и разархивировать zip файл со [страницы репозитория на GitHub](https://github.com/bpmbpm/SemanticBPM)
+
+Либо [установить git](https://git-scm.com/book/ru/v2/Введение-Установка-Git) и скачать репозиторий через консоль:
+
+`git clone https://github.com/bpmbpm/SemanticBPM.git`
+
+### Редактируем диаграмму
+
+- Устанавливаем редактор yEd ([страница скачивания инсталлятора](https://www.yworks.com/products/yed/download))
+- Открываем редактором файл `samples/vad_2/vad_2.graphml`
+- Добавляем палитру VAD:
+  - В меню программы: `Edit - Manage Palette`
+  - В панели `Available Palette Sections` выбрать `VAD2`
+  - Кликаем по `Export Session`
+  - `Close` окно Palette Manager
+  - Палитра появилась справа на панели `Palette` (проскролльте весь список палитр донизу, чтобы увидеть ее)
+  - Теперь в палитре можно выделить текущий элемент и начать редактировать схему
+- Редактируем схему, например добавляем элементы и связи из палитры VAD2
+- Сохраняем изменения нажатием Ctrl-S
+
+Диаграмма в редакторе yEd выглядит примерно так:
+
+<img src="../docs/vad_2_in_yed.png" width="600" />
+
+### Устанавливаем инструментарий
+
+Для запуска скрипта необходимо иметь
+- установленный [python 3.11](https://www.python.org/downloads/)
+- утилиту [Taskfile](https://taskfile.dev/installation/)
+  - Для Windows предварительно нужно установить менеджер пакетов [Chocolatey](https://chocolatey.org/install)
+- опционально - [валидатор Turtle](https://github.com/IDLabResearch/TurtleValidator)
+
+### Транслируем диаграмму в RDF (Turtle)
+
+Переходим в директорию yed_based_semantizer и набираем в консоли команду
+
+`task parse_vad`
+
+В результате чего в директории `samples/vad_2` появится файл `vad_2.ttl` (или перезапишется новый поверх существующего)
+
+Это валидный rdf, с которым можно проводить различные интересные операции, например загружать в triplstore и искать бизнес-паттерны через sparql-запросы (Какие - для примера напишем в следующих главах повествования).
+
+Так rdf выглдяит в редакторе онтологий [Protege](https://protege.stanford.edu/download/protege/4.3/installanywhere/Web_Installers/):
+
+<img src="../docs/vad_ttl_in_protege_1.png" width="600" />
---
+++ yed_based_semantizer/parsers/parse_vad.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+from lxml import etree
+from pprint import pp
+from argparse import ArgumentParser
+from odysseus.debug import __
+from odysseus.coll import get
+
+namespaces = {"g": "http://graphml.graphdrawing.org/xmlns", "y": "http://www.yworks.com/xml/graphml"}
+
+nodes_xpath = '//g:graph/g:node'
+edges_xpath = '//g:graph/g:edge'
+
+rdf_mapping = {
+    "comment": "rdfs:comment"
+}
+
+node_configuration_mapping = {
+    "com.yworks.flowchart.userMessage": "vad:Process",
+    "com.yworks.flowchart.start1": "vad:Performer",
+    "com.yworks.flowchart.process": "vad:Comment"
+}
+
+def prefix_header(namespace = "http://example.org/vad2/diagram#"):
+    ttl_header = f"""@prefix owl: <http://www.w3.org/2002/07/owl#> .
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix sh: <http://www.w3.org/ns/shacl#> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+@prefix vad: <http://example.org/vad2#> .
+@prefix : <{namespace}> .
+"""
+    return ttl_header
+
+def standalone_str(s):
+    return f"""
+{s}
+"""
+
+def get_node_by_id(nodes, id):
+    for node in nodes:
+        if node["id"] == id:
+            return node
+
+def is_individual(node):
+    return get(node, "type") in ["vad:Process", "vad:Performer"]
+
+def is_literal(node):
+    return get(node, "type") in ["vad:Comment"]
+
+def get_node_parameters(node):
+    return {
+        "id": node.xpath("@id", namespaces = namespaces)[0],
+        "label": node.xpath("g:data/y:GenericNode/y:NodeLabel/text()", namespaces = namespaces)[0],
+        # "type": shape_type_mapping[node.xpath("g:data/y:ShapeNode/y:Shape/@type", namespaces = namespaces)[0]],
+        "type": node_configuration_mapping[node.xpath("g:data/y:GenericNode/@configuration", namespaces = namespaces)[0]],
+    }
+
+def get_edge_parameters(edge):
+    id = edge.xpath("@id", namespaces = namespaces)[0]
+    source = edge.xpath("@source", namespaces = namespaces)[0]
+    target = edge.xpath("@target", namespaces = namespaces)[0]
+    label = edge.xpath(f"g:data/y:PolyLineEdge/y:EdgeLabel/text()", namespaces = namespaces)[0]
+    return {
+        "id": id,
+        "source": source,
+        "target": target,
+        "label": label,
+    }
+
+def build_definitions(nodes, edges):
+    individuals = [node for node in nodes if is_individual(node)]
+    result = ""
+    for i in individuals:
+        id = i["id"]
+        type = i["type"]
+        label = i["label"]
+        result += standalone_str("")
+        result += f":{id} a owl:NamedIndividual, {type} ;\n"
+        result += f"\t\trdfs:label \"{label}\" ."
+    return result
+
+def build_triples(nodes, edges):
+    result = ""
+    for edge in edges:
+        subject = get_node_by_id(nodes, edge["source"])["id"]
+        predicate = get(rdf_mapping, edge["label"], ":" + edge["label"])
+        object = get_node_by_id(nodes, edge["target"])
+        if is_literal(object):
+            object = "\"" + object["label"] + "\""
+        else:
+            object = ":" + object["id"]
+        result += standalone_str(f":{subject} {predicate} {object} .")
+    return result
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--input", action="store", dest="input")
+    parser.add_argument("-o", "--output", action="store", dest="output")
+    parser.add_argument("-n", "--namespace", action="store", dest="namespace", default="http://example.org/vad2/diagram#")
+    args = parser.parse_args()
+
+    tree = etree.parse(args.input)
+
+    nodes = tree.xpath(nodes_xpath, namespaces = namespaces)
+    edges = tree.xpath(edges_xpath, namespaces = namespaces)
+
+    processed_nodes = [get_node_parameters(node) for node in nodes]
+    processed_edges = [get_edge_parameters(edge) for edge in edges]
+
+    # triples = [build_triple(edge, processed_nodes) for edge in processed_edges]
+
+    result = prefix_header(args.namespace)
+    result += ""
+    result += build_definitions(processed_nodes, processed_edges)
+    result += "\n"
+    result += build_triples(processed_nodes, processed_edges)
+
+    # pp(result)
+    open(args.output, 'w').write(result)
