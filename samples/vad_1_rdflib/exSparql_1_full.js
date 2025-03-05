6e97fe3f8b4484784a655e9571682e3fece9ada7
Dmitry <121898072+bpmbpm@users.noreply.github.com>
2025-03-05 14:18:02 +0300

    Add files via upload

---
+++ samples/vad_1_rdflib/exSparql_1_full.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+const rdf = require('rdflib');
+
+const store = rdf.graph(); // Создаем граф для хранения данных
+// const ttlFilePath = path.resolve(__dirname, '../Process_1.ttl'); // Формруем путь к Process_1.ttl из папки на уровень выше
+const ttlFilePath = path.resolve(__dirname, 'Process_1.ttl'); 
+const ttlData = fs.readFileSync(ttlFilePath, 'utf8').toString(); // Загружаем TTL файл Process_1.ttl
+// Парсим данные из TTL файла
+rdf.parse(ttlData, store, ttlFilePath, 'text/turtle'); // Используем 'text/turtle'
+
+// Проверка.  Вывод количества триплетов в store после загрузки данных, чтобы убедиться, что данные загружены корректно: 
+console.log(`Загружено триплетов: ${store.length}`);  // length
+
+// Выводим содержимое store
+store.statements.forEach(statement => {
+    console.log(`Триплет: ${statement.subject.value} ${statement.predicate.value} ${statement.object.value}`);
+});
+
+ console.log('Start SPARQL');
+// Определяем SPARQL запрос. Универсальный запрос из тестового примера
+/*
+ const query = `
+  PREFIX ex: <http://example.org/>
+  SELECT ?s ?p ?o
+  WHERE {
+    ?s ?p ?o .
+  }
+`;
+*/
+ 
+const sparqlQuery = `
+PREFIX : <http://example.org/EKG/all_processes#>
+PREFIX vad: <http://example.org/semanter/vad#>
+SELECT DISTINCT ?s1 WHERE { ?s1 vad:hasNext :Process_1_3 .}  
+`;
+// SELECT DISTINCT ?s1 WHERE { ?s1 rdfs:type vad:ProcessType .}
+                const query2 = rdf.SPARQLToQuery(sparqlQuery, false, store);
+
+                store.query(query2, function(result) {
+                    console.log('query ran');
+//                  console.log(result);  // Вывод всех параметров узла
+                    console.log(result['?s1'].value);  //   OK     paramURI = result['?s'].value;
+                });
+           
