082e3dc943604f58b308187238db90dc5c202a41
Dmitry <121898072+bpmbpm@users.noreply.github.com>
2025-03-15 22:12:06 +0300

    Add files via upload

---
+++ samples/vad_3_comunica/com_store1_distinct.mjs
@@ -0,0 +1,59 @@
+import { QueryEngine } from '@comunica/query-sparql-file';
+import { Store } from 'n3';
+import fs from 'fs/promises';
+import { Parser } from 'n3';
+// import { Store, Parser } from 'n3';
+const myEngine = new QueryEngine();
+
+// Создаем in-memory store
+const store = new Store();
+
+// Функция для загрузки TriG-файлов в store
+async function loadTriGFileIntoStore(filePath, store) {
+  const parser = new Parser({ format: 'application/trig' });
+  const fileContent = await fs.readFile(filePath, 'utf-8');
+  const quads = parser.parse(fileContent);
+  store.addQuads(quads);
+}
+
+// Загружаем TriG-файлы в store
+await loadTriGFileIntoStore('process1.trig', store);
+await loadTriGFileIntoStore('process2.trig', store);
+
+// Выполняем SPARQL-запрос к store
+const bindingsStream = await myEngine.queryBindings(`
+  PREFIX : <http://example.org/vad/dia#>
+  SELECT DISTINCT ?s ?p ?o ?g
+    WHERE {
+    GRAPH ?g {        
+      ?s :hasNext :p1.3.
+      ?s ?p ?o
+    } 
+  } LIMIT 7`, {
+  sources: [store], // Используем store как источник данных
+});
+
+// Consume results as a stream (best performance)
+bindingsStream.on('data', (binding) => {
+    console.log(binding.toString()); // Quick way to print bindings for testing
+
+    console.log(binding.has('s')); // Will be true
+
+    // Obtaining values
+    console.log(binding.get('s').value);
+    console.log(binding.get('s').termType);
+    console.log(binding.get('p').value);
+    console.log(binding.get('o').value); // Теперь это будет работать
+    console.log(binding.get('g').value); // Для TriG
+});
+bindingsStream.on('end', () => {
+    // The data-listener will not be called anymore once we get here.
+});
+bindingsStream.on('error', (error) => {
+    console.error(error);
+});
+
+// Consume results as async iterable (easier)
+for await (const binding of bindingsStream) {
+  console.log(binding.toString());
+}
